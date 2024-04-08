2ab56ac2372846e4717f6703d7be31ef45894cac
sophiedannery <dannery.sophie@gmail.com>
2024-04-08 16:45:12 +0200

    Ajout Gestion Formulaire Inscription - Champs obligatoire

---
+++ js/auth/signup.js
@@ -0,0 +1,92 @@
+// RECUPERATION DES DONNEES
+const inputNom = document.getElementById("NomInput");
+const inputPrenom = document.getElementById("PrenomInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidatePassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
+
+
+// APPEL DES FONCTIONS 
+inputNom.addEventListener("keyup", validateForm);
+inputPrenom.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidatePassword.addEventListener("keyup", validateForm);
+
+
+// VERIFIER LE FORMULAIRE
+function validateForm() {
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPrenom);
+    const mailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
+    const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidatePassword)
+
+    if (nomOk && prenomOk && mailOk && passwordOk && passwordConfirmOk) {
+        btnValidation.disabled = false;
+    } else {
+        btnValidation.disabled = true;
+    }
+}
+
+function validateRequired(input){
+    if (input.value != '') {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
+}
+
+// VERIFIER MAIL
+function validateMail(input){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+
+    if (mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
+}
+
+
+// VERIFIER MOT DE PASSE
+function validatePassword(input){
+    const PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const PasswordUser = input.value;
+
+    if (PasswordUser.match(PasswordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
+}
+
+// VALIDER LA CONFIRMATION DU MOT DE PASSE
+function validateConfirmationPassword(inputPwd, inputConfirmPwd){
+    if (inputPwd.value == inputConfirmPwd.value){
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    } else {
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
+        return false;
+    }
+}
+
+
+
