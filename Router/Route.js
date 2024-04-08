6d0e979ca36d9490167b3a024a7878f32ca5b4c6
sophiedannery <dannery.sophie@gmail.com>
2024-04-08 19:47:50 +0200

    Ajout Gestion Authentification Routage

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,23 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+
+// Definition de l'attribut authorize
+
+/*
+[] -> tout le monde peut y accéder
+["disconnected"] -> Réserver aux utilisateurs déconnecté
+["client"] -> Réserver aux utilisateurs avec le rôle client
+["admin"] -> Réserver aux utilisateurs avec le rôle admin
+["client", "admin"] -> Réserver aux utilisateurs avec le rôle client OU admin 
+
+donc 5 cas possible 
+
+*/
